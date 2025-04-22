e972c8a8a571976e1656a181a5a617e9e8a8e988
Ekuh <esteban.kuh@gmail.com>
2025-04-21 21:25:30 -0600

    Password generator app v1.1
    Improve logic and style

--- passgen/views.py
+++ passgen/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, HttpResponse
-from django.http import JsonResponse
-import random
+import string
+import secrets
 
 # Create your views here.
 def home(req):
     return render(req, 'home.html')
 
 def passgen(req):
-    char = list('abcdefghijklmnopqrstuvwxyz')
+    char = list(string.ascii_lowercase)
     if req.GET.get('uppercase'):
-        char.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        char.extend(list(string.ascii_uppercase))
 
     if req.GET.get('digits'):
-        char.extend(list('0123456789'))
+        char.extend(list(string.digits))
 
     if req.GET.get('symbol'):
         char.extend(list('+-*/%&!@$~^'))
 
     length = int(req.GET.get('length', 10))
+    if length <= 0:
+        return HttpResponse("Longitud debe ser un número positivo")
 
-    pswd = ""
-    for x in range(length):
-        pswd += random.choice(char)
+    pswd = ''.join(secrets.choice(char) for _ in range(length))
     
     return render(req, 'password.html',  {'pswd': pswd})
